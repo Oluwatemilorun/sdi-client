66f5d985054c1a84d8c989db4863004a14896547
Isaac Oluwatemilorun <akiolisa@gmail.com>
2019-07-02 14:32:23 +0100

    Updated overview page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,37 @@
 <template>
 	<v-layout row wrap>
-		
+		<v-flex xs12 md4>
+			<v-card hover>
+				<v-card-text class="text-xs-right">
+					<div class="display-1">10</div>
+					<div class="font-weight-light">Projects</div>
+				</v-card-text>
+			</v-card>
+		</v-flex>
+		<v-flex xs12 md4>
+			<v-card hover>
+				<v-card-text class="text-xs-right">
+					<div class="display-1">10</div>
+					<div class="font-weight-light">Assignments</div>
+				</v-card-text>
+			</v-card>
+		</v-flex>
+		<v-flex xs12 md4>
+			<v-card hover>
+				<v-card-text class="text-xs-right">
+					<div class="display-1">10</div>
+					<div class="font-weight-light">Apps</div>
+				</v-card-text>
+			</v-card>
+		</v-flex>
+		<v-flex xs12 md4>
+			<v-card hover>
+				<v-card-text class="text-xs-right">
+					<div class="display-1">10</div>
+					<div class="font-weight-light">Users</div>
+				</v-card-text>
+			</v-card>
+		</v-flex>
 	</v-layout>
 </template>
 
