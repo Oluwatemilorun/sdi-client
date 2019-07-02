378ca627bdc3af3b8a1be7185af35f70f28278c4
Isaac Oluwatemilorun <akiolisa@gmail.com>
2019-07-02 14:42:01 +0100

    project page added

---
+++ pages/projects/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-layout row wrap>
+		
+
+		<v-btn color="accent" fixed right bottom round large><v-icon left medium>mdi-plus</v-icon> Add Project</v-btn>
+	</v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
